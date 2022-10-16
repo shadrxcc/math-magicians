ccaeb4763d77e4be0076f97a749246fbebd62535
shadrach akaade <akaadesm@gmail.com>
2022-10-17 00:06:39 +0100

    install babel dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import App from './components/App';
+import renderer from "react-test-renderer";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("If it renders correctly", () => {
+    const app = renderer
+        .create(<App />)
+        .toJSON();
+        expect(app).toMatchSnapshot();
 });
